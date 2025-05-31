0059ac74d9ddadf619b117d5c96a555a2d6f74ae
Sai Adithya <adithya261004@gmail.com>
2025-05-31 18:27:57 +0530

    added social media section

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AnimatedIntroSection from './components/AnimatedIntroSection';
+import SocialMediaSection from './components/SocialMediaSection';
 import PricingSection from './components/Pricing/PricingSection';
 import ContactForm from './components/Contact/ContactForm';
 import Footer from './components/Footer';
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <AnimatedIntroSection />
+      <SocialMediaSection />
       <PricingSection />
       <ContactForm />
       <Footer />
